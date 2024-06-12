7b123892edb8387a8bbf71b51527b3f9676d67bd
Yuanqing Wang <wangyq@wangyq.net>
2024-06-12 00:17:40 -0400

    diffusion implementation

--- rye/diffusion/diffusion.py
+++ rye/diffusion/diffusion.py
@@ -7,10 +7,12 @@ class Diffusion(torch.nn.Module):
             self,
             model: torch.nn.Module,
             schedule: Callable = schedule.cosine,
+            steps: int = 100,
     ):
         super().__init__()
         self.model = model
         self.schedule = schedule
+        self.steps = steps
 
     def loss(
             self,
@@ -35,7 +37,50 @@ class Diffusion(torch.nn.Module):
         epsilon_hat = self.model(x_noised)
         loss = (epsilon_hat - epsilon).pow(2).mean()
         return loss
-
+    
+    def p_mean_variance(
+            self,
+            x: torch.Tensor,
+            t: torch.Tensor,
+            t_next: torch.Tensor,
+    ):
+        log_snr = self.schedule(t)
+        log_snr_next = self.schedule(t_next)
+        c = torch.special.expm1(log_snr_next - log_snr)
+        alpha, sigma = log_snr.sigmoid().sqrt(), (-log_snr).sigmoid().sqrt()
+        alpha_next, sigma_next = log_snr_next.sigmoid().sqrt(), (-log_snr_next).sigmoid().sqrt() 
+        epsilon_hat = self.model(x)
+        x_start = (x - sigma * epsilon_hat) / alpha
+        x_start.clamp_(min=-1, max=1)
+        p_mean = alpha_next * (x * (1 - c) / alpha + c * x_start)
+        p_variance = sigma_next.pow(2) * c
+        return p_mean, p_variance
+    
+    @torch.no_grad()
+    def p_sample(
+        self,
+        x: torch.Tensor,
+        t: torch.Tensor,
+        t_next: torch.Tensor,
+    ):
+        p_mean, p_variance = self.p_mean_variance(x, t, t_next)
+        if t_next == 0:
+            return p_mean
+        epsilon = torch.randn_like(x)
+        return p_mean + epsilon * p_variance.sqrt()
+    
+    @torch.no_grad()
+    def p_sample_loop(
+        self,
+        shape: torch.Size,
+    ):
+        x = torch.randn(shape, device=self.model.device)
+        steps = torch.linspace(
+            1.0, 0.0, self.steps + 1, device=self.model.device
+        )
+        for i in range(self.steps):
+            x = self.p_sample(x, steps[i], steps[i + 1])
+        return x
 
 
 
--- rye/diffusion/schedule.py
+++ rye/diffusion/schedule.py
@@ -1,8 +1,16 @@
 import torch
 import math
 
+def right_pad_dims_to(x, t):
+    padding_dims = x.ndim - t.ndim
+    if padding_dims <= 0:
+        return t
+    return t.view(*t.shape, *((1,) * padding_dims))
+
+def log(t, eps = 1e-20):
+    return torch.log(t.clamp(min = eps))
 
 def cosine(t, logsnr_min = -15, logsnr_max = 15):
     t_min = math.atan(math.exp(-0.5 * logsnr_max))
     t_max = math.atan(math.exp(-0.5 * logsnr_min))
-    return -2 * torch.log(torch.tan(t_min + t * (t_max - t_min)))
+    return -2 * log(torch.tan(t_min + t * (t_max - t_min)))
