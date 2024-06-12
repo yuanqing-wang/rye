8c800ac6ebeae9a664279de8187a98f70e314149
Yuanqing Wang <wangyq@wangyq.net>
2024-06-12 01:15:30 -0400

    make callable an argument

--- rye/diffusion/diffusion.py
+++ rye/diffusion/diffusion.py
@@ -5,17 +5,16 @@ from . import schedule
 class Diffusion(torch.nn.Module):
     def __init__(
             self,
-            model: torch.nn.Module,
             schedule: Callable = schedule.cosine,
             steps: int = 100,
     ):
         super().__init__()
-        self.model = model
         self.schedule = schedule
         self.steps = steps
 
     def loss(
             self,
+            model: Callable,
             x: torch.Tensor,
             **kwargs,
     ):
@@ -35,12 +34,13 @@ class Diffusion(torch.nn.Module):
         x_noised =  x * alpha + epsilon * sigma
 
         # calculate the loss
-        epsilon_hat = self.model(x_noised, **kwargs)
+        epsilon_hat = model(x_noised, **kwargs)
         loss = (epsilon_hat - epsilon).pow(2).mean()
         return loss
     
     def p_mean_variance(
             self,
+            model: Callable,
             x: torch.Tensor,
             t: torch.Tensor,
             t_next: torch.Tensor,
@@ -51,7 +51,7 @@ class Diffusion(torch.nn.Module):
         c = torch.special.expm1(log_snr_next - log_snr)
         alpha, sigma = log_snr.sigmoid().sqrt(), (-log_snr).sigmoid().sqrt()
         alpha_next, sigma_next = log_snr_next.sigmoid().sqrt(), (-log_snr_next).sigmoid().sqrt() 
-        epsilon_hat = self.model(x, **kwargs)
+        epsilon_hat = model(x, **kwargs)
         x_start = (x - sigma * epsilon_hat) / alpha
         x_start.clamp_(min=-1, max=1)
         p_mean = alpha_next * (x * (1 - c) / alpha + c * x_start)
@@ -61,11 +61,12 @@ class Diffusion(torch.nn.Module):
     @torch.no_grad()
     def p_sample(
         self,
+        model: Callable,
         x: torch.Tensor,
         t: torch.Tensor,
         t_next: torch.Tensor,
     ):
-        p_mean, p_variance = self.p_mean_variance(x, t, t_next)
+        p_mean, p_variance = self.p_mean_variance(model, x, t, t_next)
         if t_next == 0:
             return p_mean
         epsilon = torch.randn_like(x)
@@ -74,14 +75,15 @@ class Diffusion(torch.nn.Module):
     @torch.no_grad()
     def p_sample_loop(
         self,
+        model: Callable,
         shape: torch.Size,
     ):
-        x = torch.randn(shape, device=self.model.device)
+        x = torch.randn(shape, device=self.device)
         steps = torch.linspace(
-            1.0, 0.0, self.steps + 1, device=self.model.device
+            1.0, 0.0, self.steps + 1, device=self.device
         )
         for i in range(self.steps):
-            x = self.p_sample(x, steps[i], steps[i + 1])
+            x = self.p_sample(model, x, steps[i], steps[i + 1])
         return x
 
 
